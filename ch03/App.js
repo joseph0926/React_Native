f66484163b3ba686a6563d2cdb878e0f835827e7
김영훈 <joseph0926@naver.com>
2022-10-11 19:15:32 +0900

    2022.10.11

---
+++ ch03/App.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+import StartGameScreen from './screens/StartGameScreen';
+
+export default function App() {
+  return (
+    <StartGameScreen></StartGameScreen>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
