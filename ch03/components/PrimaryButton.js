01cf0f688aa60ee6e1ac3b30538aa05b09f6f9ef
김영훈 <joseph0926@naver.com>
2022-10-19 15:35:53 +0900

    2022.10.19

--- ch03/components/PrimaryButton.js
+++ ch03/components/PrimaryButton.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import {View, Text} from "react-native"
+import React from "react";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 const PrimaryButton = (props) => {
-  return (
-    <View>
-      <Text>{props.children}</Text>
-    </View>
-  )
-}
+  const pressHandler = () => {};
 
-export default PrimaryButton
+  return (
+    <View style={styles.btn_outer_container}>
+      <Pressable
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+        style={({ pressed }) => {
+          return pressed ? [styles.btn_inner_container, styles.pressed] : styles.btn_inner_container;
+        }}
+      >
+        <Text style={styles.btn_Text}>{props.children}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  btn_outer_container: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+    backgroundColor: "#72063c",
+    width: 150,
+    height: 40,
+  },
+  btn_inner_container: {
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    elevation: 2,
+  },
+  btn_Text: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
+
+export default PrimaryButton;
--- ch03/screens/StartGameScreen.js
+++ ch03/screens/StartGameScreen.js
@@ -1,16 +1,53 @@
-import React from 'react'
-import {TextInput, View} from "react-native"
+import React from "react";
+import { TextInput, View, StyleSheet } from "react-native";
 
-import PrimaryButton from '../components/PrimaryButton'
+import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
-      <TextInput />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+    <View style={styles.start_container}>
+      <TextInput style={styles.textInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false} />
+      <View style={styles.btnContainer}>
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default StartGameScreen
+const styles = StyleSheet.create({
+  start_container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#4e0329",
+    paddingHorizontal: 12,
+    marginTop: 100,
+    marginHorizontal: 24,
+    paddingBottom: 16,
+    borderRadius: 10,
+    elevation: 10,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 1,
+  },
+  textInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    fontWeight: "bold",
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    textAlign: "center",
+  },
+  btnContainer: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 10,
+  },
+});
+
+export default StartGameScreen;
